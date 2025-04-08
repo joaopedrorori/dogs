ceab17b890af961c7952beb0d0660631205af144
joaopedrorori <joaopedrorr.df@gmail.com>
2025-04-08 18:25:53 -0300

    login page done

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,20 +5,23 @@ import LoginCreate from "./LoginCreate.jsx";
 import LoginPasswordLost from "./LoginPasswordLost.jsx";
 import LoginPasswordReset from "./LoginPasswordReset.jsx";
 import UserContext from "../../UserContext.jsx";
+import styles from "./Login.module.css";
 
 const Login = () => {
   const { login } = React.useContext(UserContext);
 
   if (login === true) return <Navigate to="/account" />;
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="create" element={<LoginCreate />} />
-        <Route path="lost" element={<LoginPasswordLost />} />
-        <Route path="reset" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="create" element={<LoginCreate />} />
+          <Route path="lost" element={<LoginPasswordLost />} />
+          <Route path="reset" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -4,6 +4,9 @@ import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
 import { UserContext } from "../../UserContext.jsx";
+import Error from "../Helper/Error.jsx";
+import styles from "./LoginForm.module.css";
+import stylesBtn from "../Forms/Button.module.css";
 
 const LoginForm = () => {
   const username = useForm();
@@ -20,20 +23,29 @@ const LoginForm = () => {
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" {...username} />
-        <Input label="Senha" type="password" name="password" {...password} />
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <Input label="User" type="text" name="username" {...username} />
+        <Input label="Password" type="password" name="password" {...password} />
         {loading ? (
           <Button disabled>Loading...</Button>
         ) : (
           <Button>LogIn</Button>
         )}
 
-        {error && <p>Invalid user or password.</p>}
+        {error && <Error error={error + "Invalid user or password"} />}
       </form>
-      <Link to="/login/criar">Register</Link>
+      <Link className={styles.resetPassword} to="/login/reset">
+        I forgot my password
+      </Link>
+      <div className={styles.signUp}>
+        <h2 className={styles.subtitle}>Sign Up</h2>
+        <p>Not a member yet? </p>
+        <Link className={stylesBtn.button} to="/login/register">
+          Sign up!!
+        </Link>
+      </div>
     </section>
   );
 };
